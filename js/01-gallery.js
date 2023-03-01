3f54c8a7988566231d935d7cd669cb4aeac2a8f3
Oleksandr-Kharkiv <115045030+Oleksandr-Kharkiv@users.noreply.github.com>
2023-03-02 00:29:09 +0100

    task-1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,31 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+// console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
 
-console.log(galleryItems);
+const markup = galleryItems
+  .map(({preview, original, description}) => `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"/>
+        </a>
+        </div>`).join("");
+
+galleryEl.insertAdjacentHTML("beforeend", markup);
+console.log(galleryEl);
+
+galleryEl.addEventListener('click', getUrlBigPhoto);
+
+function getUrlBigPhoto(event) {
+  event.preventDefault();
+  const urlImgOriginalSize = event.target.dataset.source;
+  console.log(urlImgOriginalSize)
+
+const instance = basicLightbox.create(`
+    <img src="${urlImgOriginalSize}" width="800" height="600">
+`)
+instance.show()
+}
+
