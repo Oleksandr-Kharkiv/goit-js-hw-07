0fc49e760ef5c63e92651904bfd0e5f871314958
Oleksandr-Kharkiv <115045030+Oleksandr-Kharkiv@users.noreply.github.com>
2023-03-06 01:38:51 +0100

    done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,39 +1,42 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 console.log(galleryItems);
-const galleryEl = document.querySelector('.gallery');
+const galleryEl = document.querySelector(".gallery");
 
 const markup = galleryItems
-  .map(({preview, original, description}) => `<div class="gallery__item">
+  .map(
+    ({ preview, original, description }) => `<div class="gallery__item">
         <a class="gallery__link" href="${original}">
             <img class="gallery__image"
             src="${preview}"
             data-source="${original}"
             alt="${description}"/>
         </a>
-        </div>`).join("");
+        </div>`
+  )
+  .join("");
 
 galleryEl.insertAdjacentHTML("beforeend", markup);
 
-galleryEl.addEventListener('click', handleModalOpen);
+galleryEl.addEventListener("click", handleModalOpen);
 
 function handleModalOpen(event) {
   event.preventDefault();
-  if(event.target.nodeName !== "IMG"){
-    return
+  if (event.target.nodeName !== "IMG") {
+    return;
   }
-const instance = basicLightbox.create(`
+  const instance = basicLightbox.create(`
     <img src="${event.target.dataset.source}" width="800" height="600">
 `);
-instance.show();
+  instance.show();
 
-window.addEventListener('keydown', handleModalEscClose);
+  window.addEventListener("keydown", handleModalEscClose);
 
-function handleModalEscClose(event) {
-  console.log(event);
-  if(event.code === "Escape") {
-    instance.close();
-    window.removeEventListener('keydown', handleModalEscClose)
-    } 
+  function handleModalEscClose(event) {
+    console.log(event);
+    if (event.code === "Escape") {
+      instance.close();
+      window.removeEventListener("keydown", handleModalEscClose);
+    }
   }
 }
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,19 +1,30 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
-const galleryOldEl = document.querySelector('.gallery');
+const galleryOldEl = document.querySelector(".gallery");
 galleryOldEl.remove();
 const galleryEl = document.createElement("div");
-galleryEl.classList.add('gallery');
-const scriptEl = document.querySelector('script')
+galleryEl.classList.add("gallery");
+const scriptEl = document.querySelector("script");
 scriptEl.before(galleryEl);
 
 const markup = galleryItems
-.map(({preview, original, description}) => `<a class="gallery__item" href="${original}">
+  .map(
+    ({
+      preview,
+      original,
+      description,
+    }) => `<a class="gallery__item" href="${original}">
 <img class="gallery__image" src="${preview}" alt="${description}" />
-</a>`).join("");
+</a>`
+  )
+  .join("");
 
 galleryEl.insertAdjacentHTML("beforeend", markup);
 
-const lightbox = new SimpleLightbox('.gallery__item', {captionsData: "alt", captionPosition: 'bottom', captionDelay: 250});
+const lightbox = new SimpleLightbox(".gallery__item", {
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
